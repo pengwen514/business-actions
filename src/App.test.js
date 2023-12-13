aa62b74ca09451bee9421d0b70165385d9032c08
B319574 <Penelope.Huff@BNSF.com>
2023-12-13 14:58:27 -0600

    solution 2: CI

--- src/App.test.js
+++ src/App.test.js
@@ -10,3 +10,7 @@ import { App } from './App';
 it('should do something amazing', () => {
 	expect(true).toBe(true);
   });
+
+it('should do something amazing', () => {
+    expect(true).toBe(false);
+});
